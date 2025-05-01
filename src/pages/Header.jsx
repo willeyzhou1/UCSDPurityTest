4b44e5fac10d46fb5bda8961329a88b8975fc2a5
willeyzhou1 <willzhou000@gmail.com>
2025-04-30 22:24:21 -0700

    minor minor update ;)

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,7 +4,7 @@ import ucsdLogo from '../assets/ucsdlogo.png';
 export default function Header({ visible }) {
   return (
     <div className='block -mt-12'>
-      <a className='flex justify-center transition hover:scale-105' href='https://www.ucsd.edu/'>
+      <a className='flex justify-center transition hover:scale-105' href='https://www.ucsd.edu/' target='_blank'>
           <img className='h-78 w-78 p-8'src={ucsdLogo}></img>
           <h1 className='text-7xl font-semibold pt-48 px-4 font-serif'>Purity Test</h1>
       </a>
