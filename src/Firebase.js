dd8ae04ced77d0ffaf1effe485194034bc7649ce
willeyzhou1 <willzhou000@gmail.com>
2025-05-18 22:06:59 -0700

    testing

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,14 +8,14 @@ import { getDatabase } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_K,
-  authDomain: import.meta.env.VITE_A_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-  databaseURL: import.meta.env.VITE_DATABASE_URL
+  apiKey: import.meta.env.API_K,
+  authDomain: import.meta.env.A_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
+  databaseURL: import.meta.env.DATABASE_URL
 };
 
 // Initialize Firebase
